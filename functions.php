09e019295eb34c79c983bf1a1fe6b96d390c86a4
Elizeu <elizeu_os@hotmail.com>
2022-09-13 13:23:11 -0300

    Add flickity files

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+//Add css
+wp_enqueue_style( 'flickity.min.css', '/flickity/flickity.min.css' )
+
+//Add js
+wp_enqueue_script( 'flickity.min.js', '/flickity/flickity..min.js', true );
---
+++ version.php
@@ -0,0 +1,7 @@
+<?php
+/**
+* Plugin Name: Flickty Slider For Wordpress
+* Description: Plugin to apply flickity slider in wordpress: https://flickity.metafizzy.co/
+* Version: 0.0.1
+* Author: Elizeu Oliveira da Silva
+**/
